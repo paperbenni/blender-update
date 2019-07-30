8fa3ad75365ba9c095d5145bfb86d5cec3571e68
paperbenni <paperbenni@gmail.com>
2019-07-30 13:19:07 +0200

    restructure installer

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-cd $HOME
-mkdir -p workspace/blender-update
-cd workspace/blender-update
-wget https://raw.githubusercontent.com/paperbenni/blender-update/master/2.79.sh
-wget https://raw.githubusercontent.com/paperbenni/blender-update/master/2.80.sh
-wget https://raw.githubusercontent.com/paperbenni/blender-update/master/Blender2.70.svg
-wget https://raw.githubusercontent.com/paperbenni/blender-update/master/Blender2.80.svg
+LINK="https://raw.githubusercontent.com/paperbenni/blender-update/master"
+cd
+mkdir blender-update
+cd blender-update
+wget "$LINK/2.79.sh"
+wget "$LINK/2.80.sh"
+wget "$LINK/2.81.sh"
+wget "$LINK/Blender2.79.svg"
+wget "$LINK/Blender2.80.svg"
+wget "$LINK/Blender2.81.svg"
 
 chmod +x *.sh
 
-cd $HOME
-
+cd
 cd ./.local/share/applications
-wget https://raw.githubusercontent.com/paperbenni/blender-update/master/blender27.desktop
-wget https://raw.githubusercontent.com/paperbenni/blender-update/master/blender28.desktop
+wget "$LINK/blender2.79.desktop"
+wget "$LINK/blender2.80.desktop"
+wget "$LINK/blender2.81.desktop"
