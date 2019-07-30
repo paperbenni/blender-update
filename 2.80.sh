1fcd101a0e5fb7c194707b74065e5ca03ce38283
paperbenni <paperbenni@gmail.com>
2019-07-30 13:07:38 +0200

    bugfix

--- 2.80.sh
+++ 2.80.sh
@@ -4,7 +4,7 @@ if [ -e .cache/blender8/blender/blender ]; then
     exit 0
 fi
 
-curl google.com || exit 1
+curl cht.sh > /dev/null || exit 1
 command -v wget || exit 1
 
 cd
@@ -13,9 +13,9 @@ cd .cache/blendercache
 echo "downloading blender"
 wget https://ftp.halifax.rwth-aachen.de/blender/release/Blender2.80/blender-2.80rc3-linux-glibc217-x86_64.tar.bz2
 tar -xvjf *.tar.bz2
-mv ./blender* ./blender
-mv ./blender ../blender8
-cd ~/.cache
+rm *.tar.bz2
+mv ./blender* ../blender8
+cd ..
 chmod +x blender8/blender
 rm -rf blendercache
-~/.cache/blender8/blender/blender
+~/.cache/blender8/blender
