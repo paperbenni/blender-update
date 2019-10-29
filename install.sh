dffede658e6861c30d49fddc0ec6298d5ac7b51b
paperbenni <paperbenni@gmail.com>
2019-10-29 10:10:18 +0100

    installer updates existing installations

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@ LINK=https://github.com/paperbenni/blender-update.git
 mkdir workspace
 cd workspace
 
-git clone --depth=1 "$LINK"
-cd blender-update
+if ! [ -e blender-update ]; then
+    git clone --depth=1 "$LINK"
+    cd blender-update
+else
+    cd blender-update
+    git pull
+fi
+
 chmod +x *.sh
 mv *.desktop ~/.local/share/applications
