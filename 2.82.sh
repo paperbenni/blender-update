7a4ee773d85c22570652c50138208308db084bb4
paperbenni <paperbenni@gmail.com>
2019-10-29 10:05:53 +0100

    new logos and blender 2.81 script, installer rewrite

---
+++ 2.82.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if ! wget --version; then
+	echo "please install wget to proceed"
+fi
+
+pushd ~/
+if ! ls ~/.cache/blender82; then
+	mkdir -p .cache/blender82
+fi
+
+if ! curl google.com; then
+	echo "curl not installed or no internet"
+	if [ -e .cache/blender82/blender/blender ]; then
+		.cache/blender82/blender/blender
+	else
+		exit 0
+	fi
+else
+	echo internet >.cache/blender82/internet.txt
+fi
+
+if [ -e .cache/blender82/internet.txt ]; then
+	echo "checking for updates"
+	cd .cache/blender82
+	rm internet.txt
+	if ! ls *.tar.bz2; then
+		touch blender.tar.bz2
+	fi
+	CURRENT=$(ls *.tar.bz2)
+	if curl https://builder.blender.org/download/ | grep "$CURRENT"; then
+		echo "up to date"
+	else
+		if ls *.tar.bz2; then
+			rm *.tar.bz2
+		fi
+		echo "updating blender"
+		wget -r --no-parent -A 'blender-2.82*linux*x86_64.tar.bz2' https://builder.blender.org/download/
+		cd builder.blender.org/download
+		tar -xvjf *.tar.bz2
+		mv *.tar.bz2 ../../
+		mv ./blender* ./blender
+		rm -rf ../../blender
+		mv ./blender ~/.cache/blender82
+		cd ../..
+		chmod +x blender/blender
+		mv ./*.tar.bz2 ~/.cache/blender82
+	fi
+	~/.cache/blender82/blender/blender
+fi
+
+popd
--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-LINK="https://raw.githubusercontent.com/paperbenni/blender-update/master"
 cd
-mkdir blender-update
+LINK=https://github.com/paperbenni/blender-update.git
+mkdir workspace
+cd workspace
+
+git clone --depth=1 "$LINK"
 cd blender-update
-wget "$LINK/2.79.sh"
-wget "$LINK/2.80.sh"
-wget "$LINK/2.81.sh"
-wget "$LINK/Blender2.79.svg"
-wget "$LINK/Blender2.80.svg"
-wget "$LINK/Blender2.81.svg"
-
 chmod +x *.sh
-
-cd
-cd ./.local/share/applications
-wget "$LINK/blender2.79.desktop"
-wget "$LINK/blender2.80.desktop"
-wget "$LINK/blender2.81.desktop"
+mv *.desktop ~/.local/share/applications
