8bc423512a85ace64fab25f1bdc605ec9102cffc
paperbenni <paperbenni@gmail.com>
2021-03-13 13:23:28 +0100

    update blender version

--- updater.sh
+++ updater.sh
@@ -4,39 +4,47 @@
 #########################################################
 
 if pgrep wget || pgrep blender || [ -e /tmp/blenderupdating ]; then
-	notify-send "the blender updater is already running"
-	echo "another instance already running"
-	exit 1
+    notify-send "the blender updater is already running"
+    echo "another instance already running"
+    exit 1
 fi
 
 if ! command -v wget &>/dev/null; then
-	echo "please install wget to proceed"
+    command -v instantinstall && {
+        instantinstall wget || exit 1
+    }
+    echo "please install wget to proceed"
+    exit 1
 fi
 
-[ -e ~/.cache/blender83 ] || mkdir -p ~/.cache/blender83
-cd ~/.cache/blender83 || exit
-curl -s 'https://builder.blender.org/download/' >download.html
-CURRENTVERSION="$(grep -o 'blender-2.83-[a-zA-Z0-9]*-linux64.tar.xz' download.html)"
+[ -e ~/.cache/blender93 ] || mkdir -p ~/.cache/blender93
+cd ~/.cache/blender93 || exit
+
+echo "checking for updates"
+CURRENTVERSION="$(
+    curl -s https://builder.blender.org/download/ | grep -o '/download/blender[^"]*tar.xz"' |
+        grep linux | tail -1 | grep -o '[^"]*' | grep -o '[^/]*$'
+)"
 
 if [ -e curversion ]; then
-	if [ "$CURRENTVERSION" = "$(cat curversion)" ]; then
-		echo "already up to date"
-	fi
+    if [ "$CURRENTVERSION" = "$(cat curversion)" ]; then
+        echo "already up to date"
+    fi
 else
-	echo "updating blender, please wait. "
-	notify-send "updating blender, please wait. Do not open another instance"
-	touch /tmp/blenderupdating
-	rm -rf ./*blender*
-	rm -rf ./blender*
-	wget https://builder.blender.org/download/"$CURRENTVERSION"
-	echo "$CURRENTVERSION" >curversion
-	mv *.tar.xz blender.tar.xz
-	notify-send "extracting blender archive"
-	tar -xf blender.tar.xz
-	rm blender.tar.xz
-	mv blender* blender
-	chmod +x ./blender/blender
-	rm /tmp/blenderupdating
+    echo "updating blender, please wait. "
+    notify-send "updating blender, please wait. Do not open another instance"
+    touch /tmp/blenderupdating
+    rm -rf ./*blender*
+    rm -rf ./blender*
+    wget https://builder.blender.org/download/"$CURRENTVERSION"
+    echo "$CURRENTVERSION" >curversion
+    mv ./*.tar.xz blender.tar.xz
+    notify-send "extracting blender archive"
+    tar -xf blender.tar.xz
+    rm blender.tar.xz
+    mv blender* blender
+    chmod +x ./blender/blender
+    rm /tmp/blenderupdating
 fi
 
 ./blender/blender
